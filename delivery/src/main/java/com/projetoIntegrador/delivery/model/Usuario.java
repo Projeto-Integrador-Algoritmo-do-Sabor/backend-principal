cc7715aa29177a70051ee14d22ee665ca07f04a3
Kevin Schuab <kevinschuab@gmail.com>
2025-01-31 14:26:25 -0300

    fix

--- delivery/src/main/java/com/projetoIntegrador/delivery/model/Usuario.java
+++ delivery/src/main/java/com/projetoIntegrador/delivery/model/Usuario.java
@@ -1,23 +1,14 @@
 package com.projetoIntegrador.delivery.model;
 
-import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Table(name = "tb_usuarios")
